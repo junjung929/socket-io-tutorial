69a47a657d28b95e4b88ddddf617e8e67057f7a7
Jun Jung <juny929@gmail.com>
2021-11-01 16:18:41 +0200

    Emitting messages

--- index.js
+++ index.js
@@ -15,4 +15,8 @@ const io = socket(server);
 
 io.on('connection', function (socket) {
   console.log('made socket connection', socket.id);
+
+  socket.on('chat', function (data) {
+    io.sockets.emit('chat', data);
+  });
 });
