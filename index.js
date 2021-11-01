628ea357b75c9feaadf21bbe037c6ad553dfac72
Jun Jung <juny929@gmail.com>
2021-11-01 16:02:04 +0200

    Using socket.io

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const socket = require('socket.io');
 
 // App setup
 const app = express();
@@ -8,3 +9,10 @@ const server = app.listen(4000, () => {
 
 // Static files
 app.use(express.static('public'));
+
+// Socket setup
+const io = socket(server);
+
+io.on('connection', function (socket) {
+  console.log('made socket connection', socket.id);
+});
